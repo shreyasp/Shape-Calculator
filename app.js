4e14b26f92a19551925150dc7bf7dec6949cf7dc
shreyas <shreyas@erpnext.com>
2016-11-13 14:44:46 +0530

    [Major] Area calculation

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var hbs = require('express-handlebars');
 var path = require('path');
-
-var parser = require(path.join(__dirname, 'parser'));
 var body_parser =  require('body-parser');
+
+// custom module imports
+var parser = require(path.join(__dirname, 'parser'));
+var area = require(path.join(__dirname, 'calc_area'));
 
 var app = express();
 
@@ -19,28 +21,20 @@ app.get('/', function(req, res, next){
   res.render('1');
 });
 
-// app.param('shape', function(req, res, next){
-//   var shape = req.param.shape;
-//   console.log(shape);
-//   if(!shape){
-//     alert('Please select a shape to proceed with calculations');
-//   }
-//
-//   next();
-// });
+// need to validation param middleware to handle no selection
 
 app.post('/2', function(req, res, next){
   var shape = req.body.shape;
   var context = parser.parse_shape(shape);
-
-  console.log(context);
   res.render('2', context);
 });
 
 // need to validation param middleware to handle no selection
 
 app.post('/3', function(req, res, next){
-  res.render('3');
+  var shape = req.body.shape;
+  var context = area.calculate_area(shape.toLowerCase(), req.body);
+  res.render('3', context);
 });
 
 // Redirection to other Routes
---
+++ calc_area.js
@@ -0,0 +1,38 @@
+module.exports = {
+  calculate_area: function(shape, dimensions){
+    var context = {
+      shape: undefined,
+      shape_properties: undefined,
+      calculated_area: 0.0
+    };
+
+    if (shape === 'rectangle') {
+      context.shape = shape;
+      context.shape_properties = 'length of ' + dimensions.Length + ' and width of ' + dimensions.Width;
+      context.calculated_area = dimensions.Length * dimensions.Width ;
+      return context;
+    }
+
+    else if (shape === 'circle') {
+      context.shape = shape;
+      context.shape_properties = 'diameter of ' + dimensions.Diameter;
+      context.calculated_area = (3.142 * Math.pow((dimensions.Diameter/2), 2));
+      return context;
+    }
+
+    else if (shape === 'square') {
+      context.shape = shape;
+      context.shape_properties = 'length of ' + dimensions.Length;
+      context.calculated_area = Math.pow(dimensions.length, 2);
+      return context;
+    }
+
+    else {
+      // Calculate area of ellipse
+      context.shape = shape;
+      context.shape_properties = 'length of ' + dimensions.Length + 'and width of ' + dimensions.Width;
+      context.calculate_area = (3.142 * dimensions.length * dimensions.width);
+      return context;
+    }
+  }
+};
